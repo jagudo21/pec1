9a00341c463879ed0e8adb0fe5a03c89779d2c93
Iosu <iosuagudo@hotmail.com>
2021-03-26 16:24:19 +0100

    Construir maqueta

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -5,12 +5,3 @@
 
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
-
-/**
- * Write any other JavaScript below
- */
-
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
